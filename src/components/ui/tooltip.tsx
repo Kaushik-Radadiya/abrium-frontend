b60e741eeeb088077a1fdadfad615d4a0256aafa
Kaushik-Radadiya <kaushikradadiya1995@gmail.com>
2026-03-16 15:34:31 +0530

    feat: prevent onclick in tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -39,16 +39,26 @@ function TooltipContent({
 }: React.ComponentProps<typeof TooltipPrimitive.Content> & {
     showArrow?: boolean
 }) {
+    const { onPointerDown, onClick, ...contentProps } = props
+
     return (
         <TooltipPrimitive.Portal>
             <TooltipPrimitive.Content
                 data-slot="tooltip-content"
                 sideOffset={sideOffset}
                 className={cn(
-                    "data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-[state=delayed-open]:animate-in data-[state=delayed-open]:fade-in-0 data-[state=delayed-open]:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 rounded-md px-3 py-1.5 text-xs bg-[var(--neutral-background-strong)] text-[var(--neutral-background)] z-50 max-w-60",
+                    "data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-[state=delayed-open]:animate-in data-[state=delayed-open]:fade-in-0 data-[state=delayed-open]:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 pointer-events-auto rounded-md px-3 py-1.5 text-xs bg-[var(--neutral-background-strong)] text-[var(--neutral-background)] z-50 max-w-60",
                     className
                 )}
-                {...props}
+                onPointerDown={(event) => {
+                    onPointerDown?.(event)
+                    event.stopPropagation()
+                }}
+                onClick={(event) => {
+                    onClick?.(event)
+                    event.stopPropagation()
+                }}
+                {...contentProps}
             >
                 {children}
                 {showArrow ? (
